2b84f47742612b1b52b52356b71d5e184d2cca83
Cash TingHin Lo <cash.raistlin@gmail.com>
2022-12-26 02:01:42 +0900

    Create infer.py

---
+++ infer.py
@@ -0,0 +1,24 @@
+import argparse
+import tensorflow as tf
+import librosa
+
+# Parse command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--model_path', type=str, required=True, help='Path to the trained model')
+parser.add_argument('--input_path', type=str, required=True, help='Path to the input audio file')
+parser.add_argument('--output_path', type=str, required=True, help='Path to the output audio file')
+args = parser.parse_args()
+
+# Load the trained model
+model = tf.keras.models.load_model(args.model_path)
+
+# Load the input audio file
+waveform, sr = librosa.load(args.input_path)
+waveform_tensor = tf.convert_to_tensor(waveform, dtype=tf.float32)
+
+# Use the model to generate a prediction
+prediction = model(waveform_tensor)
+prediction = prediction.numpy()
+
+# Save the prediction to an audio file
+librosa.output.write_wav(args.output_path, prediction, sr)
