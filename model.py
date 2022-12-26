52a8cbcae0e01aa01a08f8f8b786579ddddada19
Cash TingHin Lo <cash.raistlin@gmail.com>
2022-12-27 00:47:43 +0900

    input shape fixes (not needed?)

--- model.py
+++ model.py
@@ -1,11 +1,13 @@
 import tensorflow as tf
 
 class WaveNetModel(tf.keras.Model):
-  def __init__(self, num_filters=64, kernel_size=3):
+  def __init__(self, num_filters=64, kernel_size=3, input_shape=(None, 1), dtype=tf.float32):
     super(WaveNetModel, self).__init__()
     self.num_filters = num_filters
     self.kernel_size = kernel_size
     
+    self.input_layer = tf.keras.layers.Conv1D(filters=num_filters, kernel_size=kernel_size,
+          padding="same", activation="relu", input_shape=input_shape)
     self.conv_layers = []
     self.dense_layers = []
     for i in range(num_filters):
@@ -18,6 +20,7 @@ class WaveNetModel(tf.keras.Model):
       
   def call(self, inputs):
     x = inputs
+    x = self.input_layer(x)
     for i in range(self.num_filters):
       x = self.conv_layers[i](x)
       x = self.dense_layers[i](x)
